41c8fd8a58d54b643ee81c30f661f7dcfe3bbdae
Ken Rice <krice@freeswitch.org>
2015-05-11 14:10:05 -0500

    add a script to auto-gen a source tarball

---
+++ src_tarball.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+ORIGDIR=`pwd`
+TMPDIR=broadvoice.$$
+
+mkdir -p ../${TMPDIR}
+
+./autogen.sh
+
+cd ..
+cp -a libbroadvoice ${TMPDIR}/broadvoice-0.1.0
+cd ${TMPDIR}
+rm -rf broadvoice-0.1.0/.git*
+tar zcvf broadvoice-0.1.0.tar.gz broadvoice-0.1.0
+mv broadvoice-0.1.0.tar.gz ${ORIGDIR}/.
